24beb974a2b4c86b24797d44f8fbb2db960b4f8e
Rodrigo Nahid <69832627+rodrigonahid@users.noreply.github.com>
2021-10-16 12:28:46 -0300

    Update Me.jsx

--- components/Me.jsx
+++ components/Me.jsx
@@ -19,7 +19,7 @@ export function Me({data}){
             <div className="mt-8">
               <Link href="https://wa.me/5524981125127">
                 <a target="_blank" className="inline bg-blue-500 text-white text-lg px-6 py-2 rounded font-bold shadow hover:shadow-lg transition">
-                  Me contrate!
+                  Fale comigo!
                 </a>
               </Link>
             </div>
