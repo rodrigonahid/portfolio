45f47d74354b722d2ec261bd11dd65475fe6fd1e
rodrigonahid <rodrigonahid1@gmail.com>
2021-10-25 09:52:30 -0300

    remocao script google analytics

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -16,23 +16,7 @@ class MyDocument extends Document {
           <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
           {/* DevIcons */}
           <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.14.0/devicon.min.css" />
-          {/* Global site tag (gtag.js) - Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          />
+         
           <meta name="description" content="Me chamo Rodrigo Nahid, sou desenvolvedor Javascript com foco em React" />
           <link rel="canonical" href="https://rodrigonahid.com.br" /> 
           <meta name="keywords" content="rodrigo, nahid, desenvolvedor, dev, javascript, web" />
