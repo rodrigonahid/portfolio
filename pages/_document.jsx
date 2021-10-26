6eba4e949e3876829b001a11ce15240d607558a3
rodrigonahid <rodrigonahid1@gmail.com>
2021-10-26 15:54:37 -0300

    lazy loaded devicon cdn

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,9 +1,9 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
   }
 
   render() {
@@ -12,23 +12,40 @@ class MyDocument extends Document {
         <Head>
           {/* Google Fonts */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
-          <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
           {/* DevIcons */}
-          <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.14.0/devicon.min.css" />
-         
-          <meta name="description" content="Me chamo Rodrigo Nahid, sou desenvolvedor Javascript com foco em React" />
-          <link rel="canonical" href="https://rodrigonahid.com.br" /> 
-          <meta name="keywords" content="rodrigo, nahid, desenvolvedor, dev, javascript, web" />
-          <meta name="robots" content="index, follow" /> 
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.14.0/devicon.min.css"
+            onLoad="this.media='all'"
+          />
+
+          <meta
+            name="description"
+            content="Me chamo Rodrigo Nahid, sou desenvolvedor Javascript com foco em React"
+          />
+          <link rel="canonical" href="https://rodrigonahid.com.br" />
+          <meta
+            name="keywords"
+            content="rodrigo, nahid, desenvolvedor, dev, javascript, web"
+          />
+          <meta name="robots" content="index, follow" />
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
